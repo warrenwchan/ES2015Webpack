7e62bc50bdc5160eee2f51090021c36420d8fa6d
warrenchan <w.warrenchan@gmail.com>
2017-02-08 11:36:48 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import hello from './script1';
+import hello2 from './script2';
+
+hello();
+console.log(hello);
---
+++ src/script1.js
@@ -0,0 +1,5 @@
+function hello(){
+    console.log ('hello there');
+}
+
+export default hello;
